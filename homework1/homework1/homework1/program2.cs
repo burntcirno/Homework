faba662c93332506cff645fa884c8080169db514
burntcirno <43243611+burntcirno@users.noreply.github.com>
2019-01-13 17:48:22 +0800

    Add files via upload

---
+++ homework1/homework1/homework1/program2.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace homework1
+{
+    class Program2
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("请输入第一个整数：");
+            int a = Convert.ToInt32(Console.ReadLine());
+            Console.Write("请输入第二个整数：");
+            int b = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("a + b：" + (a + b).ToString());
+            Console.WriteLine("a * b：" + (a * b).ToString());
+
+        }
+    }
+}
